f5dcb84eb125fa3120c5168aa52ab6b9e0da927f
Cameron Makarchuk <cmakarchuk@gmail.com>
2022-11-01 18:02:11 -0400

    Set up LeagueDetails component files

---
+++ src/components/LeagueDetails/LeagueDetails.js
@@ -0,0 +1,14 @@
+import './LeagueDetails.scss';
+
+
+export default function LeagueDetails() {
+
+    return (
+        <section className='league-details'>
+            <h2 className='league-details__title'>League Name</h2>
+
+
+
+        </section>
+    )
+}
