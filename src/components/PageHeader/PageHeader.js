b4f83bdd00bd8b1ca4a75b6ef978cf967801961d
Cameron Makarchuk <cmakarchuk@gmail.com>
2022-11-02 09:09:12 -0400

    Create api.js in utils folder with getAllLeagues api request

--- src/components/PageHeader/PageHeader.js
+++ src/components/PageHeader/PageHeader.js
@@ -5,11 +5,11 @@ export default function PageHeader() {
 
     return (
         <header className='page-header'>
-            <Link to='' className='page-header__link'><h3 className='page-header__logo'>RecLeague</h3></Link>
+            <Link to='/' className='page-header__link'><h3 className='page-header__logo'>RecLeague</h3></Link>
 
             <nav className='page-header-nav'>
                 <ul className='page-header-nav__list'>
-                    <NavLink to='' className='page-header-nav__link'><li className='page-header-nav__list-item'>All Leagues</li></NavLink>
+                    <NavLink to='/all-leagues' className='page-header-nav__link'><li className='page-header-nav__list-item'>All Leagues</li></NavLink>
                 </ul>
 
                 <button className='page-header-nav__login-button'>Login/Register</button>
---
+++ src/utils/api.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:8080';
+
+export const getAllLeagues = () => {
+    return axios.get(`${API_URL}/leagues`);
+};
+
