86f3bebbc8c191616eb0aa0d798293e94829ef53
Cameron Makarchuk <cmakarchuk@gmail.com>
2022-11-02 10:23:03 -0400

    Create LeagueCard with data from getAllLeagues api request

--- src/components/AllLeaguesList/AllLeaguesList.js
+++ src/components/AllLeaguesList/AllLeaguesList.js
@@ -1,13 +1,44 @@
 import LeaguesCard from '../LeaguesCard/LeaguesCard';
 import './AllLeaguesList.scss';
+import '../../utils/api';
+import { useState, useEffect } from 'react';
+import { getAllLeagues } from '../../utils/api';
 
 export default function AllLeaguesList() {
+    const [leagues, setLeagues] = useState(undefined);
+
+    useEffect(() => {
+        getAllLeagues()
+            .then(({ data }) => {
+                setLeagues(data);
+            })
+            .catch(err => `Could not retrieve leagues: ${err}`);
+    }, []);
+
+    if (!leagues) {
+        return <p>Loading...</p>;
+    }
+
 
     return (
 
         <section className='all-leagues-list'>
 
-            <LeaguesCard />
+            {leagues.map(league => {
+                return (
+                    <LeaguesCard
+                        leagueId={league.id}
+                        leagueName={league.name}
+                        leagueSport={league.sport}
+                        leagueStart={league.start_date}
+                        leagueAddress={league.address}
+                        leagueCity={league.city} />
+                )
+            })}
+
+
+
+
         </section>
     )
 }
--- src/components/LeaguesCard/LeaguesCard.js
+++ src/components/LeaguesCard/LeaguesCard.js
@@ -1,30 +1,33 @@
 import './LeaguesCard.scss';
 import leaguePlaceholder from '../../assets/images/league-placeholder.jpg';
+import { Link } from 'react-router-dom';
 
 
-export default function LeaguesCard() {
+export default function LeaguesCard({ leagueId, leagueName, leagueSport, leagueStart, leagueAddress, leagueCity }) {
+
 
     return (
-        <section className='leagues-card'>
+        <Link to={`/leagues/${leagueId}`} className='leagues-card__link'>
+            <section className='leagues-card'>
 
-            <img src={leaguePlaceholder} className='leagues-card__image' alt='league picture' />
-            <div className='leagues-card__content'>
+                <img src={leaguePlaceholder} className='leagues-card__image' alt='league picture' />
+                <div className='leagues-card__content'>
 
+                    <div className='leagues-card__info'>
+                        <h3 className='leagues-card__name'>{leagueName}</h3>
+                        <p className='leagues-card__sport'>{leagueSport}</p>
+                        <p className='leagues-card__start-date'>{leagueStart}</p>
+                    </div>
 
-                <div className='leagues-card__info'>
-                    <h3 className='leagues-card__name'>League Name</h3>
-                    <p className='leagues-card__sport'>Sport</p>
-                    <p className='leagues-card__start-date'>Start Date</p>
+                    <div className='leagues-card__address'>
+                        <p className='leagues-card__street'>{leagueAddress}</p>
+                        <p className='leagues-card__city'>{leagueCity}</p>
+                    </div>
+
                 </div>
 
-                <div className='leagues-card__address'>
-                    <p className='leagues-card__street'>Street Address</p>
-                    <p className='leagues-card__city'>City</p>
-                </div>
 
-            </div>
-
-
-        </section>
+            </section>
+        </Link>
     )
 }
