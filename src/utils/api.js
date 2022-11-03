e39904cf19b3b48088e3549c1e33386658d89b35
Cameron Makarchuk <cmakarchuk@gmail.com>
2022-11-03 18:42:35 -0400

    Setup loginUser POST request in api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,23 +1,15 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080';
+export const API_URL = 'http://localhost:8080';
 
-export const getAllLeagues = () => {
-    return axios.get(`${API_URL}/leagues`);
-};
+export const getAllLeagues = () => axios.get(`${API_URL}/leagues`);
 
-export const getSearchResults = (location, sport) => {
-    return axios.get(`${API_URL}/leagues/?location=${location}&sport=${sport}`);
-};
+export const getSearchResults = (location, sport) => axios.get(`${API_URL}/leagues/?location=${location}&sport=${sport}`);
 
-export const getLeagueById = (leagueId) => {
-    return axios.get(`${API_URL}/leagues/${leagueId}`);
-}
+export const getLeagueById = (leagueId) => axios.get(`${API_URL}/leagues/${leagueId}`);
 
-export const createNewUser = (newUser) => {
-    return axios.post(`${API_URL}/users`, newUser);
-}
+export const createNewUser = (newUser) => axios.post(`${API_URL}/users`, newUser);
 
-export const createNewLeague = (newLeague) => {
-    return axios.post(`${API_URL}/leagues`, newLeague)
-}
+export const createNewLeague = (newLeague) => axios.post(`${API_URL}/leagues`, newLeague);
+
+export const loginUser = (loginDetails) => axios.post(`${API_URL}/auth/login`, loginDetails);
