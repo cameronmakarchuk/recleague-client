a6347ec831a261d2752bd5cb4fa1e7cd876eacdf
Cameron Makarchuk <cmakarchuk@gmail.com>
2022-11-03 20:55:08 -0400

    Populate ProfilePage with user data when logged in

--- src/pages/ProfilePage/ProfilePage.js
+++ src/pages/ProfilePage/ProfilePage.js
@@ -19,28 +19,47 @@ export default function ProfilePage() {
         const email = e.target.login_email.value;
         const password = e.target.login_password.value;
         loginUser({ email, password })
-            .then(resp => {
-                console.log(resp);
+            .then((resp) => {
+                if (resp.status === 403) {
+                    setIsLoginError(true);
+                    setErrorMessage(resp.error.message);
+                } else {
+                    const bearerToken = resp.data.token;
+                    sessionStorage.bearerToken = bearerToken;
+                    setIsLoggedIn(true);
+                    setIsLoginError(false);
+                    setErrorMessage('');
+                    setProfileData(resp.data.user);
+                }
             })
-            .catch(err => alert(`Error loggin in: ${err}`));
+            .catch(err => alert(`Error logging in: ${err}`));
+        e.target.reset();
     }
 
 
     if (!isLoggedIn) return <LoginForm handleLogin={handleLogin} />
 
     return (
+
         <section className='profile'>
-            <h2 className='profile__title'>First Name Last Name</h2>
 
-            <img src={avatarPlaceholder} className='profile__avatar' alt='profile avatar' />
+            {profileData && (
+                <>
+                    <h2 className='profile__title'>{profileData.first_name} {profileData.last_name}</h2>
 
-            <p className='profile__text'>Email: </p>
-            <p className='profile__text'>Street Address: </p>
-            <p className='profile__text'>City: </p>
-            <p className='profile__text'>Province: </p>
-            <p className='profile__text'>Country: </p>
-            <p className='profile__text'>Postal Code: </p>
-            <p className='profile__text'>Leagues Managed: </p>
+                    <img src={avatarPlaceholder} className='profile__avatar' alt='profile avatar' />
+
+                    <p className='profile__text'><span className='profile__text profile__text--emphasis'>Email: </span>{profileData.email} </p>
+                    <p className='profile__text'><span className='profile__text profile__text--emphasis'>Street Address: </span>{profileData.address} </p>
+                    <p className='profile__text'><span className='profile__text profile__text--emphasis'>City: </span>{profileData.city} </p>
+                    <p className='profile__text'><span className='profile__text profile__text--emphasis'>Province: </span>{profileData.province} </p>
+                    <p className='profile__text'><span className='profile__text profile__text--emphasis'>Country: </span>{profileData.country} </p>
+                    <p className='profile__text'><span className='profile__text profile__text--emphasis'>Postal Code: </span>{profileData.postal_code} </p>
+                    <p className='profile__text'><span className='profile__text profile__text--emphasis'>Leagues Managed: </span></p>
+                </>
+            )}
+
         </section>
     )
+
 }
