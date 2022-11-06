a50e855f38aed610796059b95fd5aad22497376b
Cameron Makarchuk <cmakarchuk@gmail.com>
2022-11-06 18:50:16 -0500

    Add styling for ProfilePage on mobile

--- src/pages/ProfilePage/ProfilePage.js
+++ src/pages/ProfilePage/ProfilePage.js
@@ -43,22 +43,25 @@ export default function ProfilePage({ isLoggedIn, profileData, setProfileData, l
                 profileData && (
                     <>
                         <h2 className='profile__title'>{profileData.first_name} {profileData.last_name}</h2>
+                        <div className='profile__user'>
+                            <img src={`${API_URL}/${profileData.avatar_img}`} className='profile__avatar' alt='profile avatar' />
 
-                        <img src={`${API_URL}/${profileData.avatar_img}`} className='profile__avatar' alt='profile avatar' />
-
-                        <p className='profile__text'><span className='profile__text profile__text--emphasis'>Email: </span>{profileData.email} </p>
-                        <p className='profile__text'><span className='profile__text profile__text--emphasis'>Street Address: </span>{profileData.address} </p>
-                        <p className='profile__text'><span className='profile__text profile__text--emphasis'>City: </span>{profileData.city} </p>
-                        <p className='profile__text'><span className='profile__text profile__text--emphasis'>Province: </span>{profileData.province} </p>
-                        <p className='profile__text'><span className='profile__text profile__text--emphasis'>Country: </span>{profileData.country} </p>
-                        <p className='profile__text'><span className='profile__text profile__text--emphasis'>Postal Code: </span>{profileData.postal_code} </p>
+                            <div className='profile__user-details'>
+                                <p className='profile__text'><span className='profile__text profile__text--emphasis'>Email: </span>{profileData.email} </p>
+                                <p className='profile__text'><span className='profile__text profile__text--emphasis'>Street Address: </span>{profileData.address} </p>
+                                <p className='profile__text'><span className='profile__text profile__text--emphasis'>City: </span>{profileData.city} </p>
+                                <p className='profile__text'><span className='profile__text profile__text--emphasis'>Province: </span>{profileData.province} </p>
+                                <p className='profile__text'><span className='profile__text profile__text--emphasis'>Country: </span>{profileData.country} </p>
+                                <p className='profile__text'><span className='profile__text profile__text--emphasis'>Postal Code: </span>{profileData.postal_code} </p>
+                            </div>
+                        </div>
 
                         {leaguesJoined
                             ? <>
                                 <p className='profile__text profile__text--emphasis'>Leagues You're In: </p>
                                 <ul className='profile__league-list'>
                                     {leaguesJoined.map(league => {
-                                        return <li className='profile__league-list-item' key={league.id_league}><Link className='profile__text profile__league-link' to={`/leagues/${league.id_league}`}>{league.name}</Link></li>
+                                        return <li className='profile__league-list-item' key={league.id_league}><Link className='profile__league-link' to={`/leagues/${league.id_league}`}>{league.name}</Link></li>
                                     }
                                     )}
                                 </ul>
@@ -71,7 +74,7 @@ export default function ProfilePage({ isLoggedIn, profileData, setProfileData, l
                                 <p className='profile__text profile__text--emphasis'>Leagues You Manage: </p>
                                 <ul className='profile__league-list'>
                                     {leaguesByUser.map(league => {
-                                        return <li className='profile__league-list-item' key={league.id_league}><Link className='profile__text profile__league-link' to={`/leagues/${league.id_league}`}>{league.name}</Link></li>
+                                        return <li className='profile__league-list-item' key={league.id_league}><Link className='profile__league-link' to={`/leagues/${league.id_league}`}>{league.name}</Link></li>
                                     }
                                     )}
                                 </ul>
@@ -79,8 +82,10 @@ export default function ProfilePage({ isLoggedIn, profileData, setProfileData, l
                             : <p>You don't manage any leagues.</p>
                         }
 
-                        <button onClick={handleLogout} className='profile__button profile__button--logout'>Logout</button>
-                        <button onClick={() => window.location.href = '/add-league'} className='profile__button profile__button--add-league'>Post Your League</button>
+                        <div className='profile__buttons'>
+                            <button onClick={() => window.location.href = '/add-league'} className='profile__button profile__button--add-league'>Post Your League</button>
+                            <button onClick={handleLogout} className='profile__button profile__button--logout'>Logout</button>
+                        </div>
                     </>
                 )
             ) : (
